6b5e7a37c6527001462ba97f73ce1ff8f43d649c
kumarshubu <shubhamkumaar@gmail.com>
2020-02-03 13:43:18 +0530

    created redux store, combineCReducer file

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -5,12 +5,12 @@ import thunk from 'redux-thunk'
 import ProfileReducer from './Profiles/ProfileReducer'
 import IdReducer from './Ids/IdReducer.js'
 import UserReducer from './Users/UserReducer'
-
+import { composeWithDevTools } from 'redux-devtools-extension';
 const rootReducer = combineReducers({
 	Profile:ProfileReducer,
 	Id:IdReducer,
 	User:UserReducer
 })
 
-const store = createStore(rootReducer,applyMiddleware(logger,thunk))
+const store = createStore(rootReducer,composeWithDevTools(applyMiddleware(logger,thunk)))
 export default store
